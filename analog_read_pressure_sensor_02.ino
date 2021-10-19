74c4a6b23f1a2b9387bf7bc98d65a93efcda05e0
chibaf <chibaf@mac.com>
2021-10-19 22:26:04 +0900

    Add files via upload

---
+++ analog_read_pressure_sensor_02.ino
@@ -0,0 +1,22 @@
+int analogPin0 = A0; // potentiometer wiper (middle terminal) connected to analog pin 3
+int analogPin1 = A1; // outside leads to ground and +5V
+int analogPin2 = A2;
+
+int val0 = 0;  // variable to store the value read
+int val1 = 0;  // variable to store the value read
+int val2 = 0;  // variable to store the value read
+
+void setup() {
+  Serial.begin(9600);           //  setup serial
+}
+
+void loop() {
+  val0 = analogRead(analogPin0);  // read the input pin
+  val1 = analogRead(analogPin1);  // read the input pin
+  val2 = analogRead(analogPin2);  // read the input pin
+  Serial.println(val0);          // debug value
+  Serial.println(",");
+  Serial.println(val1);
+  Serial.println(",");
+  Serial.println(val2);
+}
